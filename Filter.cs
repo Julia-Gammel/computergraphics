891ad6eaf065fe85720d5ce1b81d2323514b88de
MrKiskers <etopirog@mail.ru>
2019-04-01 10:08:27 +0300

    Add files via upload

---
+++ Filter.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Test
+{
+    abstract class Filters
+    {
+        protected abstract Color calculateNewPicelColor(Bitmap sourceImage, int x, int y);
+        public int Clamp(int value, int min, int max) //цвет (3 его компонента) принимает значение от 0 до 255, чтобы при работе фильтра не выходить за рамки, мы используем эту функцию 
+        {
+            if (value < min)
+                return min;
+            if (value > max)
+                return max;
+            return value;
+        }
+        public Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
+        {
+            Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+            for (int i = 0; i < sourceImage.Width; i++)
+            {
+                worker.ReportProgress((int)(float)i / resultImage.Width * 100); //будет сигнализировать элементу BackgroundWorker о текущем прогрессе
+                if (worker.CancellationPending)
+             return null;
+            for (int j = 0; j < sourceImage.Height; j++)
+            {
+                resultImage.SetPixel(i, j, calculateNewPicelColor(sourceImage, i, j));
+            }
+        }
+            
+            return resultImage;
+        }
+
+    }
+    class InvertFilter : Filters
+    {
+        protected override Color calculateNewPicelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            Color resultColor = Color.FromArgb(255 - sourceColor.R, 255 - sourceColor.G, 255 - sourceColor.B);
+            return resultColor;
+        }
+    }
+
+    class MatrixFilter : Filters
+    {
+        protected float[,] kernel = null;
+        protected MatrixFilter() { }
+        public MatrixFilter(float[,] kernel)
+        {
+            this.kernel = kernel;
+        }
+        protected override Color calculateNewPicelColor(Bitmap sourceImage, int x, int y)
+        {
+            int radiusX = kernel.GetLength(0) / 2;
+            int radiusY = kernel.GetLength(1) / 2;
+            float resultR = 0; //переменные типа float, в которых будут храниться цветовые компоненты результирующего цвета
+            float resultG = 0;
+            float resultB = 0;
+            for (int l = -radiusY; l <= radiusY; l++) //два вложенных цикла, которые будут перебирать окрестность пикселя
+            {
+                for (int k = -radiusX; k <= radiusX; k++)
+                {   //х и y – координаты текущего пикселя
+                    //Чтобы на граничных пикселях не выйти за границы изображения, используйте функцию Clamp.
+                    //В переменных idX и idY хранятся координаты пикселей-соседей пикселя (x,y), с которым совмещается центр матрицы, 
+                                                                                              // и для которого происходит вычисления цвета
+                    int idX = Clamp(x + k, 0, sourceImage.Width - 1); //l и k принимают значения от -radius до radius и означают положение элемента в матрице фильтра(ядре), если начало отсчета поместить в центр матрицы
+                    int idY = Clamp(y + l, 0, sourceImage.Height - 1);
+                    Color neighborColor = sourceImage.GetPixel(idX, idY);
+                    resultR += neighborColor.R * kernel[k + radiusX, l + radiusY];
+                    resultG += neighborColor.G * kernel[k + radiusX, l + radiusY];
+                    resultB += neighborColor.B * kernel[k + radiusX, l + radiusY];
+                }
+            }
+            return Color.FromArgb(Clamp((int)resultR, 0, 255), Clamp((int)resultG, 0, 255), Clamp((int)resultB, 0, 255));
+        }
+    }
+
+    class BlurFilter : MatrixFilter
+    {
+        public BlurFilter()
+        {
+            int sizeX = 3;
+            int sizeY = 3;
+            kernel = new float[sizeX, sizeY];
+            for (int i = 0; i < sizeX; i++)
+                for (int j = 0; i < sizeX; j++)
+                    kernel[i, j] = 1.0f / (float)(sizeX * sizeY); //ошибка туть
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void фильтрыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e) //открыть
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.png; *.jpg; *.bmp; *.jpeg | All files (*.*) | *.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(dialog.FileName); //открыли картинку в прогу, теперь передадим её в picture Box
+            } else { 
+             //Console.WriteLine("Nani"); 
+            Application.Exit();  }
+            pictureBox1.Image = image;
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) //по клику мышки работает, ха-ха-ха
+        {
+           // pictureBox1.Image = image;
+            //pictureBox1.Refresh();
+        }
+
+        private void инверсияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InvertFilter filter = new InvertFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+           // Bitmap resultImage = filter.processImage(image);
+           // pictureBox1.Image = resultImage;
+          //  pictureBox1.Refresh();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap newImage = ((Filters)e.Argument).processImage(image, backgroundWorker1);
+            if (backgroundWorker1.CancellationPending != true)
+                image = newImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        { //будет визуализировать обработанное изображение на форме и обнулять полосу прогресса
+            if (!e.Cancelled)
+            {
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+            progressBar1.Value = 0;
+        }
+
+        private void размытиеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new BlurFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+    }
+}
